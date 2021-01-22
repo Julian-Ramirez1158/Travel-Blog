67b3a11b5d57c2eb49dcd36bd288f0825dfa5c62
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2021-01-22 00:04:32 -0500

    Add url paths for FAQ and About pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('about', views.about, name="about"),
+    path('faq', views.faq, name="faq"),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
