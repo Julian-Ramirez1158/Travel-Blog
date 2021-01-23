e8fdf17f5889e700acd8d4c6549f6c56633f330c
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2021-01-22 20:56:39 -0500

    static folder

--- myblog/settings.py
+++ myblog/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
