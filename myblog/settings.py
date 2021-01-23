cd99bd4aa438faf1537fa1986e55a3256be8a32a
Julian-Ramirez1158 <69184233+Julian-Ramirez1158@users.noreply.github.com>
2021-01-23 16:06:42 -0500

    Debut set to false

--- myblog/settings.py
+++ myblog/settings.py
@@ -31,7 +31,8 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False #os.getenv("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
